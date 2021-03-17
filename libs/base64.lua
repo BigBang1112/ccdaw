e25d446e639ad7f8951d94686909a5b86f9944e7
Petr Pivoňka <petrpiv1@gmail.com>
2021-03-18 00:28:53 +0100

    Fix identation

--- libs/base64.lua
+++ libs/base64.lua
@@ -4,12 +4,12 @@
 -- license: The Unlicense (https://unlicense.org/)
 
 base64 = {
-	table = {
-		"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
-		"Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f",
-		"g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-		"w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "/"
-	}
+    table = {
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+        "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f",
+        "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+        "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "/"
+    }
 }
 
 -- to_base64(in_handle, out_handle)
@@ -17,64 +17,64 @@ base64 = {
 --   in_handle:
 --   out_handle:
 base64.to_base64 = function (self, in_handle, out_handle)
-	local num = nil
-	repeat
-		num = nil
-		local num_size = 4 -- byte size
-		for i = 1, 3 do
-			local byte = in_handle.read()
-			if byte == nil then
-				if i ~= 1 then
-					num_size = i
-				end
-				break
-			else
-				if num == nil then
-					num = byte
-				else
-					num = bit.bor(num, bit.blshift(byte, 8 * (i - 1)))
-				end
-			end
-		end
-		if num ~= nil then
-			for i = 1, num_size do
-				local index = bit.band(bit.brshift(num, 6 * (i - 1)), 63) 
-				local byte = string.byte(self.table[index + 1])
-				out_handle.write(byte)
-			end
-		end
-	until num == nil
+    local num = nil
+    repeat
+        num = nil
+        local num_size = 4 -- byte size
+        for i = 1, 3 do
+            local byte = in_handle.read()
+            if byte == nil then
+                if i ~= 1 then
+                    num_size = i
+                end
+                break
+            else
+                if num == nil then
+                    num = byte
+                else
+                    num = bit.bor(num, bit.blshift(byte, 8 * (i - 1)))
+                end
+            end
+        end
+        if num ~= nil then
+            for i = 1, num_size do
+                local index = bit.band(bit.brshift(num, 6 * (i - 1)), 63) 
+                local byte = string.byte(self.table[index + 1])
+                out_handle.write(byte)
+            end
+        end
+    until num == nil
 end
 
 base64.from_base64 = function (self, in_handle, out_handle)
-	local indicies = nil
-	repeat
-		indicies = {}
-		for i = 1, 4 do
-			local byte = in_handle.read()
-			if byte == nil then
-				break
-			else
-				local char = string.char(byte)
-				local index = 0
-				for i, c in pairs(self.table) do
-					if char == c then
-						indicies[i] = c
-						break
-					end
-				end
-			end
-		end
-		if #indicies > 0 then
-			local num = 0
-			for i = 1, #indicies do
-				local index = indicies[i]
-				num = bit.bor(num, bit.blshift(index, 6 * (i - 1)))
-			end
-			for i = 1, #indicies do
-				local byte = bit.band(bit.brshift(num, 8 * (i - 1)), 255)
-				out_handle.write(byte)
-			end
-		end
-	until #indicies < 4
+    local indicies = nil
+    repeat
+        indicies = {}
+        for i = 1, 4 do
+            local byte = in_handle.read()
+            if byte == nil then
+                break
+            else
+                local char = string.char(byte)
+                local index = 0
+                for i, c in pairs(self.table) do
+                    if char == c then
+                        indicies[i] = c
+                        break
+                    end
+                end
+            end
+        end
+        if #indicies > 0 then
+            local num = 0
+            for i = 1, #indicies do
+                local index = indicies[i]
+                num = bit.bor(num, bit.blshift(index, 6 * (i - 1)))
+            end
+            for i = 1, #indicies do
+                local byte = bit.band(bit.brshift(num, 8 * (i - 1)), 255)
+                out_handle.write(byte)
+            end
+        end
+    until #indicies < 4
 end
--- libs/bin.lua
+++ libs/bin.lua
@@ -4,64 +4,64 @@
 -- license: The Unlicense (https://unlicense.org/)
 
 bin = {
-	handle = nil
+    handle = nil
 }
 
 bin.write_string = function (self, str, no_prefix)
-	local h = self.handle
+    local h = self.handle
 
-	if str == nil then
-		h.write(0)
-	else
-		if #str > 255 then
-			error("string longer than 255")
-		end
+    if str == nil then
+        h.write(0)
+    else
+        if #str > 255 then
+            error("string longer than 255")
+        end
 
-		if not no_prefix then
-			h.write(#str)
-		end
+        if not no_prefix then
+            h.write(#str)
+        end
 
-		for i = 1, #str do
-			local c = str:sub(i,i)
-			h.write(string.byte(c))
-		end
-	end
+        for i = 1, #str do
+            local c = str:sub(i,i)
+            h.write(string.byte(c))
+        end
+    end
 end
 
 bin.write_byte = function (self, n)
-	self.handle.write(n)
+    self.handle.write(n)
 end
 
 bin.write_short = function (self, n) ----
-	local h = self.handle
-	h.write(bit.band(n, 255))
-	h.write(bit.band(bit.brshift(n, 8), 255))
+    local h = self.handle
+    h.write(bit.band(n, 255))
+    h.write(bit.band(bit.brshift(n, 8), 255))
 end
 
 bin.read_string = function (self, length)
-	local h = self.handle
+    local h = self.handle
 
-	local len = length
-	if len == nil then
-		len = h.read()
-	end
+    local len = length
+    if len == nil then
+        len = h.read()
+    end
 
-	if len > 255 then
-		error("string longer than 255")
-	end
+    if len > 255 then
+        error("string longer than 255")
+    end
 
-	local chars = {}
-	for i = 1, len do
-		table.insert(chars, string.char(h.read()))
-	end
-	return table.concat(chars)
+    local chars = {}
+    for i = 1, len do
+        table.insert(chars, string.char(h.read()))
+    end
+    return table.concat(chars)
 end
 
 bin.read_byte = function (self)
-	return self.handle.read()
+    return self.handle.read()
 end
 
 bin.read_short = function (self)
-	local h = self.handle
-	return bit.bor(h.read(), bit.blshift(h.read(), 8))
+    local h = self.handle
+    return bit.bor(h.read(), bit.blshift(h.read(), 8))
 end
